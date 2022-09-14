36db9c01d28244cacaf264079cdd6198160a1cd5
Anass Saidi <anas.15.saidi@gmail.com>
2022-09-14 11:54:40 +0100

    creating the list view

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
         theme: ThemeData(
           colorScheme: ColorScheme.fromSwatch().copyWith(
@@ -27,20 +26,30 @@ class RandomWordsState extends State<RandomWords> {
   final _randowWordPairs = <WordPair>[];
 
   Widget _buildList() {
-    return ListView(
+    return ListView.builder(
       padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, item){
-        if(item.isOdd) return Divider();
-      }
+      itemBuilder: (context, item) {
+        // ignore: prefer_const_constructors
+        if (item.isOdd) return Divider();
+
+        final index = item ~/ 2;
+
+        if (index >= _randowWordPairs.length) {
+          _randowWordPairs.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_randowWordPairs[index]);
+      },
     );
   }
 
-  Widget build(BuildContext context){
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+        title: Text(pair.asPascalCase, style: TextStyle(fontSize: 18.0)));
+  }
+
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("WordPair Generator")
-      ),
-      body: _buildList()
-    )
+        appBar: AppBar(title: Text("WordPair Generator")), body: _buildList());
   }
 }
