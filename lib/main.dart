e3bdb84b48fe4121070c3124478057cf04db997d
Anass Saidi <anas.15.saidi@gmail.com>
2022-09-14 12:19:33 +0100

    Adding Favourite Toggles

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import './random_words.dart';
 
 // ignore: prefer_const_constructors
 void main() => runApp(MyApp());
@@ -14,42 +14,5 @@ class MyApp extends StatelessWidget {
           ),
         ),
         home: RandomWords());
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords> {
-  final _randowWordPairs = <WordPair>[];
-
-  Widget _buildList() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, item) {
-        // ignore: prefer_const_constructors
-        if (item.isOdd) return Divider();
-
-        final index = item ~/ 2;
-
-        if (index >= _randowWordPairs.length) {
-          _randowWordPairs.addAll(generateWordPairs().take(10));
-        }
-
-        return _buildRow(_randowWordPairs[index]);
-      },
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-        title: Text(pair.asPascalCase, style: TextStyle(fontSize: 18.0)));
-  }
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text("WordPair Generator")), body: _buildList());
   }
 }
---
+++ lib/random_words.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _randowWordPairs = <WordPair>[];
+  final _savedWordPairs = Set<WordPair>();
+
+  Widget _buildList() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, item) {
+        // ignore: prefer_const_constructors
+        if (item.isOdd) return Divider();
+
+        final index = item ~/ 2;
+
+        if (index >= _randowWordPairs.length) {
+          _randowWordPairs.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_randowWordPairs[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _savedWordPairs.contains(pair);
+
+    return ListTile(
+      title: Text(pair.asPascalCase, style: TextStyle(fontSize: 18.0)),
+      trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _savedWordPairs.remove(pair);
+          } else {
+            _savedWordPairs.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("WordPair Generator")), body: _buildList());
+  }
+}
